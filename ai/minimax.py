e4474aebb0e900d78c68e3387746efbe9b76c7e3
Eduardo Goncalvez <eduardogoncalvez00@gmail.com>
2025-10-18 19:09:58 -0400

    Implement base minimax algorithm

--- ai/minimax.py
+++ ai/minimax.py
@@ -1,7 +1,7 @@
 def minimax(state, depth, maxPlayer):
-    if depth == 0 or is_terminal(state):
+    if is_terminal(state) or depth == 0:
         return evaluate(state)
-    
+
     if maxPlayer:
         bestScore = float('-inf') 
         
@@ -24,17 +24,57 @@ def minimax(state, depth, maxPlayer):
 
         return bestScore
 
-def is_terminal(state): #TODO
-    return    
+def utility(state, player):
+    for row in range(3):
+        if state[row][0] == player and state[row][1] == player and state[row][2] == player:
+            return True
+        
+    for col in range(3):
+        if state[0][col] == player and state[1][col] == player and state[2][col] == player:
+            return True
+        
+    if state[0][0] == player and state[1][1] == player and state[2][2] == player:
+        return True
+    
+    if state[0][2] == player and state[1][1] == player and state[2][0] == player:
+        return True
+    
+    return False
 
-def evaluate(state): #TODO
-    return    
+def is_terminal(state):
+    return utility(state, 'X') or utility(state, 'O') or is_draw(state)    
 
-def actions(state): #TODO
-    return
+def evaluate(state):
+    if utility(state, 'X'):
+        return 10
+    elif utility(state, 'O'):
+        return -10
+    else:
+        return 0
 
-def result(state, move, sign): #TODO
-    return
+def actions(state): 
+    moves = []
+    for row in range(3):
+        for col in range(3):
+            if state[row][col] is None:
+                moves.append((row, col))
+    
+    return moves
 
-def undo_move(state, move): #TODO
-    return
+def result(state, move, sign): 
+    row, col = move
+
+    state[row][col] = sign
+
+def undo_move(state, move): 
+    row, col = move
+
+    state[row][col] = None
+
+def is_draw(state):
+    for row in range(3):
+        for col in range(3):
+            if state[row][col] is None:
+                return False
+            
+    return True
