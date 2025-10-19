7e6fe341111d813c6d27dfe86b61a3a60dc51c50
Eduardo Goncalvez <eduardogoncalvez00@gmail.com>
2025-10-18 20:46:08 -0400

    Implement functions to get the best moves

---
+++ ai/ai_player.py
@@ -0,0 +1,22 @@
+from ai.minimax import actions, result, minimax, undo_move
+from ai.alphabeta import alphabeta
+
+def get_best_move(state, depth, player, use_alphabeta=True):
+    best_move = None
+    best_score = float('-inf') if player == 'X' else float('inf')
+
+    for move in actions(state):
+        result(state, move, player)
+
+        if use_alphabeta:
+            score = alphabeta(state, depth-1, player=='O', float('-inf'), float('inf'))
+        else:
+            score = minimax(state, depth-1, player=='O')
+
+        undo_move(state, move)
+
+        if (player == 'X' and score > best_score) or (player == 'O' and score < best_score):
+            best_score = score
+            best_move = move
+
+    return best_move
